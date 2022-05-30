643d80029c13d6ac8eb39e74ee33e687c11a9044
Deepak Kumar Saxena <deepzsenu@gmail.com>
2022-05-31 04:47:09 +0530

    Create Counting Sort.py

---
+++ Hackerank/1weekofinterview/day2/Counting Sort.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+from collections import Counter
+def countingSort(arr):
+    ma = max(arr)
+    l = [0]*(ma+1)
+    j = Counter(arr)
+    for i in j:
+        l[i] = j[i]
+     
+    return l
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
