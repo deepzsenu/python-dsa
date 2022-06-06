52f31be6112afb59595cb5cb7558daf867476d2a
Deepak Kumar Saxena <deepzsenu@gmail.com>
2022-06-06 20:18:26 +0530

    Update graph repesentaion using list.py

--- Graph/graph repesentaion using list.py
+++ Graph/graph repesentaion using list.py
@@ -8,8 +8,9 @@ def printGraph(adj):
         print(e)
         
         
-from  collections import deque        
-def bsfVersionOne(adj, s):
+from  collections import deque
+# for cyclic and connected undirected graph        
+def bfsVersionOne(adj, s):
     visited = [False]*len(adj)
     q = deque()
     q.append(s)
@@ -21,6 +22,28 @@ def bsfVersionOne(adj, s):
             if visited[u] == False:
                 q.append(u)
                 visited[u] = True
+    print()
+    
+def bfs(adj, s, visited):
+    
+    q = deque()
+    q.append(s)
+    visited[s] = True
+    while q:
+        s = q.popleft()
+        print(s, end = " ")
+        for u in adj[s]:
+            if visited[u] == False:
+                q.append(u)
+                visited[u] = True
+                
+                
+# for disconnected graphs
+def Bfsin(adj):
+    visited = [False]*len(adj)
+    for u in range(len(adj)):
+        if visited[u] == False:
+            bfs(adj,u,visited)            
     print()
                 
             
@@ -36,4 +59,5 @@ addEdge(adj,1,3)
 
 printGraph(adj)
 
-bsfVersionOne(adj, 3)
+bfsVersionOne(adj, 3)
+Bfsin(adj)
